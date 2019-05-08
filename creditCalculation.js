e4f8bb9fe08140db3ef4e3a65f23c6323203f560
Leonid Krainov <KrajnovLeonid@gmail.com>
2019-05-08 15:54:38 +0300

    Create creditCalculation.js

---
+++ creditCalculation.js
@@ -0,0 +1,9 @@
+let creditCalculation = function(summ,duration,rate){
+	let overPayment = 0;
+  for (i=duration; i>0; i--) {
+  let yearOverPayment = summ * (rate/100);
+  summ = summ - (summ/duration);
+  overPayment += yearOverPayment;
+  }
+  return overPayment;
+};
