2ad0e28ab9bea95e60d9dc53cf28d98c3f565dcf
Leonid Krainov <KrajnovLeonid@gmail.com>
2019-05-15 11:28:42 +0300

    Update creditCalculation.js
    separate per month

--- creditCalculation.js
+++ creditCalculation.js
@@ -1,9 +1,12 @@
 let creditCalculation = function(summ,duration,rate){
 	let overPayment = 0;
-  for (let i = duration; i>0; i--) {
-  let yearOverPayment = summ * (rate/100);
-  summ = summ - (summ/duration);
-  overPayment += yearOverPayment;
+    let durationMonth = duration * 12;
+    let monthPay = summ/durationMonth;
+    let currentSumm = summ;
+  for (let i = durationMonth; i>0; i--) {
+    let mounthOverPayment = currentSumm * (rate/(12*100));
+    currentSumm -= monthPay
+    overPayment += mounthOverPayment;
   }
   return overPayment;
 };
