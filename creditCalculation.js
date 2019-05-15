71020f01aa8cc272034ce7eb792ec4cf10869034
Leonid Krainov <KrajnovLeonid@gmail.com>
2019-05-15 11:29:43 +0300

    Update creditCalculation.js

--- creditCalculation.js
+++ creditCalculation.js
@@ -4,9 +4,9 @@ let creditCalculation = function(summ,duration,rate){
     let monthPay = summ/durationMonth;
     let currentSumm = summ;
   for (let i = durationMonth; i>0; i--) {
-    let mounthOverPayment = currentSumm * (rate/(12*100));
+    let monthOverPayment = currentSumm * (rate/(12*100));
     currentSumm -= monthPay
-    overPayment += mounthOverPayment;
+    overPayment += monthOverPayment;
   }
   return overPayment;
 };
